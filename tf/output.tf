d2108fdcf1b4513438a33809868b0795f073fb9f
Ola Huss <olahuss@Olas-Air.attlocal.net>
2022-03-08 14:36:02 -0600

    fix

--- tf/output.tf
+++ tf/output.tf
@@ -1,9 +1,9 @@
 output "s3_bucket_id" {
   description = "The name of the bucket."
-  value       = resource.s3_bucket.demos3bucket.id
+  value       = resource.aws_s3_bucket.demos3bucket.id
 }
 
 output "s3_bucket_arn" {
   description = "The ARN of the bucket"
-  value       = resource.s3_bucket.demos3bucket.arn
+  value       = resource.aws_s3_bucket.demos3bucket.arn
 }
